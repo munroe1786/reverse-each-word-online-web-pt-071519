9daeccaf92e3096b6c5cb44952484abcb7b7c020
Eliza Munroe <era3munroe@gmail.com>
2019-07-25 17:40:13 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  array = [ ]
+  string.split.each do |word| 
+  puts array << word.reverse
+end
+  array.join(" ")
+end
+
+
+def reverse_each_word(string)
+  new_array = [ ]
+  string.split.collect do |word| 
+  new_array.join(" ") << word.reverse
+end
+end
+
+
