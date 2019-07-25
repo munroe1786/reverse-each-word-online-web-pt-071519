8c2d937241a533f188131c42f03275576ad754af
Eliza Munroe <era3munroe@gmail.com>
2019-07-25 17:47:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(string)
   original_array = string.split(" ")
   new_array = [ ]
   original_array.each do |string| 
-  new_array << word.reverse
+   new_array << word.reverse
 end
-  array.join(" ")
+  new_array.join(" ")
 end
 
 
